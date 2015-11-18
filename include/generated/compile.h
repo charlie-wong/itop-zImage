420d559f80ce9c40c96473ea94243c0c0aaf6d9a
Charlie Wong Super <1213charlie@gmail.com>
2015-11-18 19:56:35 +0800

    init

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 11 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#11 SMP PREEMPT Thu Nov 12 21:25:56 CST 2015"
+#define LINUX_COMPILE_BY "charlie"
+#define LINUX_COMPILE_HOST "ThinkPad"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-67) "
