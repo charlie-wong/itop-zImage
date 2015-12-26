d67ed6c04fc1daa79f5d34a41c614b85928cab8f
Charlie Wong Super <1213charlie@gmail.com>
2015-12-26 19:06:46 +0800

    init

--- scripts/kconfig/.zconf.tab.o.cmd
+++ scripts/kconfig/.zconf.tab.o.cmd
@@ -3,9 +3,9 @@ cmd_scripts/kconfig/zconf.tab.o := gcc -Wp,-MD,scripts/kconfig/.zconf.tab.o.d -W
 source_scripts/kconfig/zconf.tab.o := scripts/kconfig/zconf.tab.c
 
 deps_scripts/kconfig/zconf.tab.o := \
+  /usr/include/stdc-predef.h \
   /usr/include/ctype.h \
   /usr/include/features.h \
-  /usr/include/stdc-predef.h \
   /usr/include/x86_64-linux-gnu/sys/cdefs.h \
   /usr/include/x86_64-linux-gnu/bits/wordsize.h \
   /usr/include/x86_64-linux-gnu/gnu/stubs.h \
@@ -17,9 +17,9 @@ deps_scripts/kconfig/zconf.tab.o := \
   /usr/include/x86_64-linux-gnu/bits/byteswap.h \
   /usr/include/x86_64-linux-gnu/bits/byteswap-16.h \
   /usr/include/xlocale.h \
-  /usr/lib/gcc/x86_64-linux-gnu/4.4.7/include/stdarg.h \
+  /usr/lib/gcc/x86_64-linux-gnu/4.8/include/stdarg.h \
   /usr/include/stdio.h \
-  /usr/lib/gcc/x86_64-linux-gnu/4.4.7/include/stddef.h \
+  /usr/lib/gcc/x86_64-linux-gnu/4.8/include/stddef.h \
   /usr/include/libio.h \
   /usr/include/_G_config.h \
   /usr/include/wchar.h \
@@ -47,7 +47,7 @@ deps_scripts/kconfig/zconf.tab.o := \
   /usr/include/x86_64-linux-gnu/bits/string.h \
   /usr/include/x86_64-linux-gnu/bits/string2.h \
   /usr/include/x86_64-linux-gnu/bits/string3.h \
-  /usr/lib/gcc/x86_64-linux-gnu/4.4.7/include/stdbool.h \
+  /usr/lib/gcc/x86_64-linux-gnu/4.8/include/stdbool.h \
   scripts/kconfig/lkc.h \
     $(wildcard include/config/.h) \
     $(wildcard include/config/list.h) \
@@ -64,8 +64,8 @@ deps_scripts/kconfig/zconf.tab.o := \
   /usr/include/x86_64-linux-gnu/asm/errno.h \
   /usr/include/asm-generic/errno.h \
   /usr/include/asm-generic/errno-base.h \
-  /usr/lib/gcc/x86_64-linux-gnu/4.4.7/include-fixed/limits.h \
-  /usr/lib/gcc/x86_64-linux-gnu/4.4.7/include-fixed/syslimits.h \
+  /usr/lib/gcc/x86_64-linux-gnu/4.8/include-fixed/limits.h \
+  /usr/lib/gcc/x86_64-linux-gnu/4.8/include-fixed/syslimits.h \
   /usr/include/limits.h \
   /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
   /usr/include/x86_64-linux-gnu/bits/local_lim.h \
